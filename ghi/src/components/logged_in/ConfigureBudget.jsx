6eaa50103f03fd43a51193a6c9b66458819d2317
Will Tirado <wrtirado@gmail.com>
2023-09-08 07:52:09 -0600

    Built the pie chart and styled Budget View page

--- ghi/src/components/logged_in/ConfigureBudget.jsx
+++ ghi/src/components/logged_in/ConfigureBudget.jsx
@@ -83,7 +83,7 @@ const ConfigureBudget = ({ createdBudget, baseUrl }) => {
   };
 
   const handleSubmit = async (e) => {
-    handleSubmitOrSave(e, true);
+    await handleSubmitOrSave(e, true);
     const data = await FastAPI.get(`/api/budgets`, token);
     setBudgetsData(data.budgets);
     navigate(`/budgets/${createdBudget.id}`);
@@ -108,7 +108,7 @@ const ConfigureBudget = ({ createdBudget, baseUrl }) => {
 		<Avatar sx={{ m: 1, bgcolor: '#242424', justifyContent: 'center' }}>
             <WalletOutlinedIcon />
         </Avatar>
-		<Grid container direction='column' alignItems='center' justify='center'>
+		<Grid container direction='column' sx={{ alignItems:'center', justify:'center', display: 'flex' }}>
 			<Typography 
 			variant="h5" 
 			sx={{ fontWeight: "bold" }}> 
@@ -146,7 +146,6 @@ const ConfigureBudget = ({ createdBudget, baseUrl }) => {
 				label="Expense Name"
 				variant="outlined"
 				fullWidth
-				alignItems="right"
 				onChange={(e) => handleExpenseNameChange(e, index)}
 				value={expenseItems[index].expenseName}
 				type="text"
@@ -181,7 +180,6 @@ const ConfigureBudget = ({ createdBudget, baseUrl }) => {
 			))
 		)}
 
-
 			<Card sx={{ mt: 2, border: "none", boxShadow: "none"}}>
 				<Button 
 				variant="outlined"
@@ -210,15 +208,8 @@ const ConfigureBudget = ({ createdBudget, baseUrl }) => {
 				Submit
 				</Button>
 			</Card>
-
-
-
-				
 			</Box>
 		</Container>
-
-
-
   );
 };
 
