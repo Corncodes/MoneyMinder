4b788c3a137c8940187c3f4bdb0cfe4e60ac9f22
Franz Espinosa <espinosa.franz@gmail.com>
2023-09-11 16:08:53 -0400

    fixed link to login page in Create Account page

--- ghi/src/components/logged_out/CreateAccountForm.jsx
+++ ghi/src/components/logged_out/CreateAccountForm.jsx
@@ -144,7 +144,7 @@ const handleSubmit = async (e) => {
           </Button>
           <Grid container justifyContent="center">
             <Grid item>
-              <Link href="/" variant="body2">
+              <Link href="/login" variant="body2">
                 Already have an account? Sign in
               </Link>
             </Grid>
